bc5ed6ae80bc84716a287f1026c77366f028d547
Ieltzu <ieltzuirazu@gmail.com>
2015-11-05 18:18:50 +0100

    con fallos pero pensado, espero que os guste

--- bagging/src/bag/Bagging.java
+++ bagging/src/bag/Bagging.java
@@ -1,6 +1,11 @@
 package bag;
 
+import java.util.ArrayList;
+
 import src.Lector;
+import src.NuestroModelo;
+import src.NuestroModelo.DistanceWight;
+import weka.core.Instance;
 import weka.core.Instances;
 
 public class Bagging {
@@ -9,7 +14,6 @@ public class Bagging {
 	private Instances misinstancias;
 	
 	public Bagging(int l,Instances ins) {
-		bagnum = l;
 		misinstancias = ins;
 	}
 	
@@ -24,11 +28,25 @@ public class Bagging {
 	
 	public static void main(String[] args) {
 		Instances instancias = Lector.getLector().leerInstancias("./iris.arff");
-		//TODO nuevo knn
+		NuestroModelo noum= new NuestroModelo(2, DistanceWight.NoDistance, 1);
+		Instance instanciaAClasificar = Lector.getLector().leerInstancias("Como vendria la instancia?'?'");
+		double accuracyMax = 0.0,accuracy=0.0;
+		ArrayList<Integer> valVot = new ArrayList<Integer>(instancias.numClasses());
+		int numeroL = 0;
 		for (int i = 2; i <= 15; i++) {
 			Instances boost = crearMuestras(instancias);
-			Bagging nb= new Bagging(i, boost);
+			noum.buildClasifier(boost);
+			noum.evaluarModelo();
+			accuracy = noum.accuracy();
+			if(accuracy > accuracyMax){
+				accuracyMax = accuracy;
+				numeroL=i;
+			}
+			double valorClase = noum.clasificarInstancia(instanciaAClasificar);
+			//falta coger el indice por el que la instancia se ha clasificado
+			valVot.set(/*El valor de arriba*/, valVot.get(/*El valor de arriba*/));
 		}
+
 		
 	}
 	
